844911f7e5ec48601e3c0510fcb0bdf0b8ecba27
PalmForest <realpalmforest@gmail.com>
2025-06-26 15:40:44 +1000

    Created basic RoleTargetButton

---
+++ UselessRoles/Buttons/RoleTargetButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactor.Utilities;
+using Reactor.Utilities.Attributes;
+using UnityEngine;
+using UselessRoles.Utility;
+using static UnityEngine.GraphicsBuffer;
+
+namespace UselessRoles.Buttons;
+
+[RegisterInIl2Cpp]
+public class RoleTargetButton : RoleActionButton
+{
+    public float UseRange = 50f;
+    public Func<PlayerControl, bool> ValidTargets;
+
+    public Color HighlightColor = Color.red;
+
+    public PlayerControl Target
+    {
+        get => _target;
+        set
+        {
+            if (!PlayerControl.LocalPlayer || PlayerControl.LocalPlayer.Data == null || !PlayerControl.LocalPlayer.Data.Role)
+                return;
+
+            _target = value;
+
+            if (_target)
+            {
+                _target.SetOutlineColor(HighlightColor);
+                base.SetEnabled();
+            }
+            else
+            {
+                _target.SetOutlineColor(null);
+                base.SetDisabled();
+            }
+
+            Logger<UselessRolesPlugin>.Message($"Target of {this.GetType().FullName} set to {_target.name}");
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        try
+        {
+            Target = PlayerControl.LocalPlayer.FindClosestPlayer(ValidTargets);
+        }
+        catch (Exception ex) when (ex is NullReferenceException)
+        {
+            Logger<UselessRolesPlugin>.Error($"{ex.Message}\nNo predicate provided for ValidTargets of {this.GetType().FullName}");
+        }
+    }
+
+    private PlayerControl _target;
+}
--- UselessRoles/Utility/PlayerTools.cs
+++ UselessRoles/Utility/PlayerTools.cs
@@ -1,4 +1,8 @@
-﻿using UselessRoles.Roles;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UselessRoles.Roles;
 
 namespace UselessRoles.Utility;
 
@@ -18,5 +22,42 @@ public static class PlayerTools
     public static Role GetRole(this PlayerControl player)
     {
         return RoleManager.Roles[player.PlayerId];
+    }
+
+    public static void SetOutlineColor(this PlayerControl player, Color? color)
+    {
+        if(color.HasValue)
+            player.cosmetics.SetOutline(true, new Il2CppSystem.Nullable<Color>(color.Value));
+        else player.cosmetics.SetOutline(false, null);
+    }
+
+    public static PlayerControl FindClosestPlayer(this PlayerControl player, Func<PlayerControl, bool> predicate)
+    {
+        var players = PlayerControl.AllPlayerControls
+            .ToArray()
+            .Where(predicate)
+            .ToList();
+
+        Vector2 myPos = player.GetTruePosition();
+
+        players.Sort(delegate (PlayerControl a, PlayerControl b)
+        {
+            float magnitude2 = (a.GetTruePosition() - myPos).magnitude;
+            float magnitude3 = (b.GetTruePosition() - myPos).magnitude;
+            if (magnitude2 > magnitude3)
+            {
+                return 1;
+            }
+            if (magnitude2 < magnitude3)
+            {
+                return -1;
+            }
+            return 0;
+        });
+        
+        if(players.Count > 0)
+            return players[0];
+
+        return null;
     }
 }
