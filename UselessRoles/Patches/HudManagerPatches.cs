d29c18866af643d6f5594b48da47a1c092ce7461
PalmForest <realpalmforest@gmail.com>
2025-07-03 19:45:48 +1000

    New Trap button and HudManager patch fix

--- UselessRoles/Patches/HudManagerPatches.cs
+++ UselessRoles/Patches/HudManagerPatches.cs
@@ -10,21 +10,13 @@ public static class HudManagerPatches
     [HarmonyPostfix]
     public static void HudShowIntro_Postfix(HudManager __instance)
     {
-        if (!AmongUsClient.Instance.AmHost)
-            return;
-
-        foreach (var player in PlayerControl.AllPlayerControls)
-            player.GetRole().OnHudStart(__instance);
+        PlayerControl.LocalPlayer.GetRole().OnHudStart(__instance);
     }
 
     [HarmonyPatch(methodName: nameof(HudManager.SetHudActive), argumentTypes: [typeof(bool)])]
     [HarmonyPostfix]
     public static void SetHudActive_Postfix(HudManager __instance, bool isActive)
     {
-        if (!AmongUsClient.Instance.AmHost)
-            return;
-
-        foreach (var player in PlayerControl.AllPlayerControls)
-            player.GetRole().OnHudActive(__instance, isActive);
+        PlayerControl.LocalPlayer.GetRole().OnHudActive(__instance, isActive);
     }
 }
